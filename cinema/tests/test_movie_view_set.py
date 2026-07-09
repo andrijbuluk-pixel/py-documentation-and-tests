e929a49e4fd78ab7c87dde28406d94e267e17f3e
Andriy <andrijbuluk@gmail.com>
2026-07-09 15:05:57 +0200

    Aded: test for filter by title

--- cinema/tests/test_movie_view_set.py
+++ cinema/tests/test_movie_view_set.py
@@ -1,9 +1,13 @@
+from io import BytesIO
+
+from PIL import Image
 from rest_framework.test import APITestCase
 
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 from cinema.models import Movie, Genre, Actor
 from cinema.serializers import MovieListSerializer, MovieDetailSerializer
@@ -19,8 +23,10 @@ class MovieViewSetTests(APITestCase):
         )
         self.client.force_authenticate(self.user)
 
-        self.save_genres = Genre.objects.create(name="Fantasy")
-        self.save_actors = Actor.objects.create(first_name="Test", last_name="User")
+        self.save_genres_1 = Genre.objects.create(name="Fantasy")
+        self.save_genres_2 = Genre.objects.create(name="Action")
+        self.save_actors_1 = Actor.objects.create(first_name="Baki", last_name="Hanma")
+        self.save_actors_2 = Actor.objects.create(first_name="Yujiro", last_name="Hanma")
 
     def test_item_list(self) -> None:
         url = reverse("cinema:movie-list")
@@ -39,8 +45,8 @@ class MovieViewSetTests(APITestCase):
             duration=120,
         )
 
-        item.genres.set([self.save_genres])
-        item.actors.set([self.save_actors])
+        item.genres.set([self.save_genres_1])
+        item.actors.set([self.save_actors_2])
 
         url = reverse("cinema:movie-detail", args=[item.id])
         res = self.client.get(url)
@@ -55,8 +61,8 @@ class MovieViewSetTests(APITestCase):
             "title": "Test_title",
             "description": "Test_description",
             "duration": 90,
-            "genres": [self.save_genres.id],
-            "actors": [self.save_actors.id],
+            "genres": [self.save_genres_1.id],
+            "actors": [self.save_actors_2.id],
         }
 
         url = reverse("cinema:movie-list")
@@ -64,3 +70,17 @@ class MovieViewSetTests(APITestCase):
 
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Movie.objects.count(), 1)
+
+    def test_filter_movies_by_title(self) -> None:
+        movie_1 = {"title": "Matrix", "description": "Matrix", "duration": 90}
+        movie_2 = {"title": "Avatar", "description": "Avatar", "duration": 90}
+
+        Movie.objects.create(**movie_1)
+        Movie.objects.create(**movie_2)
+
+        url = reverse("cinema:movie-list")
+        res = self.client.get(url, data=movie_1)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]["title"], "Matrix")
